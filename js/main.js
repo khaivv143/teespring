6021097500d26d409b4a12de1fe79e2bfe4709b6
khaivv <khaivv@softdreams.vn>
2019-06-23 22:52:03 +0700

    login, signup, recover

---
+++ js/main.js
@@ -0,0 +1,79 @@
+function searchFocus() {
+    $('.suggestion-results').show();
+}
+
+function searchBlur() {
+    $('.suggestion-results').hide();
+}
+
+function searchKeyUp() {
+    var value = $('.action-search').val();
+    if (value.length == 0) {
+        $('.suggestion-results').show();
+    } else {
+        $('.suggestion-results').hide();
+    }
+}
+
+$('.btn-menu-mobile').on('click', function () {
+    var status = $(this).attr('rel');
+    if (status == 'open') {
+        $(this).removeClass('change-open').addClass('change-close').attr('rel', 'close');
+        $('.menu-mobile').show('fast');
+    } else {
+        $(this).removeClass('change-close').addClass('change-open').attr('rel', 'open');
+        $('.menu-mobile').hide('fast');
+    }
+})
+
+
+$('.btn-cancel').on('click', function(){
+    $('.search-modal').hide('fast');
+})
+
+$('.over-modal').on('click', function(){
+    $('.search-modal').hide('fast');
+})
+
+$('.btn-search-mobile').on('click', function(){
+    $('.search-modal').show('fast')
+})
+
+
+$('.form-login input').keyup(function(){
+    var wrap = $(this).parent();
+    var myValue = $(this).val();
+    
+    if(myValue.length != 0){
+        wrap.addClass('item-focus');
+    }else{
+        wrap.removeClass('item-focus');
+    }  
+})
+
+$('.form-signup input').keyup(function(){
+    var wrap = $(this).parent();
+    var myValue = $(this).val();
+    
+    if(myValue.length != 0){
+        wrap.addClass('item-focus');
+    }else{
+        wrap.removeClass('item-focus');
+    }  
+})
+
+$('.form-recover input').keyup(function(){
+    var wrap = $(this).parent();
+    var myValue = $(this).val();
+    
+    if(myValue.length != 0){
+        wrap.addClass('item-focus');
+    }else{
+        wrap.removeClass('item-focus');
+    }  
+})
+
+$('.authen a').on('click', function(){
+    var authen = $(this).parent();
+    authen.removeClass('authen').addClass('authen-input');
+})
